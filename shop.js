480b281e622c44e1680e16e10e5eec7a330b5779
FA1F.W <fa1994f.w@gmail.com>
2023-08-31 22:00:20 +0300

    -убрал из корзины возможность увеличивать/уменьшать колличество одной и той же услуги; (методы incrementCount & decrementCount при этом в корзине остались);

--- shop.js
+++ shop.js
@@ -3,19 +3,24 @@ const priceAll = document.querySelectorAll('.formPay__calculator__price');
 const cartBadge = cartDiv.querySelector('.cart__badge');
 
 const renderShop = () => {
-  purchases.innerHTML = cart.getAll().map((item) => `
+  purchases.innerHTML = cart
+    .getAll()
+    .map(
+      (item) => `
     <div class="row d-flex justify-content-between align-items-center mb-3">
-      <span class="col-12 col-xl-6 fw-bold mb-3 mb-xl-0">${cart.getNameItem(item.id)}</span>
-      <span class="col-5 col-xl-3 fs-5 text-xl-center">
-        <i id=${item.id} data-manager="decrementCount" class="bi bi-dash-circle me-1"></i>
-        ${cart.getCountItem(item.id)}
-        <i id=${item.id} data-manager="incrementCount" class="bi bi-plus-circle ms-1"></i>
-      </span>
-      <span class="col-5 col-xl-2 fs-6">${cart.getPriceItem(item.id) * item.count} BYN</span>
-      <i id=${item.id} data-manager="removeItem" class="bi bi-x-circle col-2 col-xl-1 fs-5 text-end"></i>
+      <span class="col-12 col-xl-6 fw-bold mb-3 mb-xl-0">${cart.getNameItem(
+        item.id,
+      )}</span>
+
+      <span class="col-5 col-xl-2 fs-6">${cart.getPriceItem(item.id)} BYN</span>
+      <i id=${
+        item.id
+      } data-manager="removeItem" class="bi bi-x-circle col-2 col-xl-1 fs-5 text-end"></i>
     </div>
-  `).join('');
-  priceAll.forEach((price) => price.textContent = cart.getPriceAll());
+  `,
+    )
+    .join('');
+  priceAll.forEach((price) => (price.textContent = cart.getPriceAll()));
   cartBadge.textContent = cart.getCountItems();
   cartDiv.dataset.bsTitle = `= ${cart.getPriceAll()} BYN`;
   if (window.screen.width > 768) {
@@ -24,16 +29,33 @@ const renderShop = () => {
 
   const managersButtons = document.querySelectorAll('[data-manager]');
   managersButtons.forEach((button) => {
-    button.addEventListener('click', ({ target: { id, dataset: { manager } }}) => {
-      cart[manager](id);
-      if (cart.getCount() === 0) {
-        closeModal();
-        return closeCart();
-      }
-      renderShop();
-    });
+    button.addEventListener(
+      'click',
+      ({
+        target: {
+          id,
+          dataset: { manager },
+        },
+      }) => {
+        cart[manager](id);
+        if (cart.getCount() === 0) {
+          closeModal();
+          return closeCart();
+        }
+        renderShop();
+      },
+    );
   });
 };
+
+/*
+it was removed from 14th
+       <span class="col-5 col-xl-3 fs-5 text-xl-center">
+        <i id=${item.id} data-manager="decrementCount" class="bi bi-dash-circle me-1"></i>
+        ${cart.getCountItem(item.id)}
+        <i id=${item.id} data-manager="incrementCount" class="bi bi-plus-circle ms-1"></i>
+      </span>
+ */
 
 const shopInit = () => {
   const buyButtons = document.querySelectorAll('.buy');
@@ -44,7 +66,7 @@ const shopInit = () => {
     }
     openModal();
 
-/*    когда будет короткое описание карточек, лучше сделать так:
+    /*    когда будет короткое описание карточек, лучше сделать так:
       таким образом, данные будут добавлены напрямую со страницы, что избавляет от проблемы
       дублирования данных при добавлении новых карточек товара в массив data.
 
@@ -58,7 +80,6 @@ const shopInit = () => {
     const service = data.find((item) => item.id === target.id);
     cart.addItem(service);
     renderShop();
-
   };
 
   cartDiv.addEventListener('click', openModal);
